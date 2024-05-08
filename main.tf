c791b71ea8d278dbb7d5f43e3c8b8f919fc6987d
Colin Coleman <colin.coleman@kontrollsmart.com>
2024-05-08 16:13:30 +0200

    Bump ecs-fargate ref

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_ecs_cluster" "cluster" {
 }
 
 module "fargate" {
-  source                             = "github.com/Cantara/terraform-aws-ecs-fargate?ref=864ef7c"
+  source                             = "github.com/Cantara/terraform-aws-ecs-fargate?ref=bc88da4"
   name_prefix                        = var.name_prefix
   vpc_id                             = var.vpc_id
   private_subnet_ids                 = var.private_subnet_ids
